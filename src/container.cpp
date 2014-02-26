3b5a3e6548d03fd4f73b4d38c8bdee48ce2af721
Anastasia <nicetrip1943@gmail.com>
2014-02-26 23:26:59 +0400

    Структура узла списка

--- src/container.cpp
+++ src/container.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-void hello_world()
-{
-    std::cout << "Hello, World!" << std::endl;
-}
+
+
+typedef struct node {
+		             char *p_value; 	// ”казатель на значение 
+		     	     node *p_next_node;	// ”казатель на следующий узел
+	                }l, *lp;
