0b419c2606c0480d296ffb314780117c14677214
Roman Maksimov <maksimov.ro.al@gmail.com>
2014-03-02 15:51:53 +0400

    Добавление и извлечение элементов

--- src/container.h
+++ src/container.h
@@ -1,3 +1,5 @@
 
 void Create_List(item*& pFirst, char* Str);  //Создание контейнера
-int Delete_List(item*&, item*&);  //Уничтожение контейнера
+int Delete_List(item*&, item*&);             //Уничтожение контейнера
+void Add_Items();                            //Добавление элементов в список
+void Extracting_Elements();                  //Извлечение элементов из списка
